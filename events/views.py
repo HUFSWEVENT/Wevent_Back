2c579be82bf05e962ca390af8d993e6cde566867
sm4640 <s1m0611@naver.com>
2023-11-22 15:26:45 +0900

    pagination size 20 수정

--- events/views.py
+++ events/views.py
@@ -22,7 +22,7 @@ from .serializers import eventBaseSerializer, adSerializer, adImageSerializer, e
 
 
 class EventSetPagination(PageNumberPagination):
-    page_size = 5
+    page_size = 20
     page_size_query_param = 'page_size'
     max_page_size = 100
 
